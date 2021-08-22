3773b923bbcfd1b9ea2dc4d24863b9c30c7469cf
LukSiman <simanavicius.lukas@gmail.com>
2021-08-22 15:35:51 -0700

    implemented adding books to the list functionality from the form input

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 const table = document.querySelector('table');
 const addBookButton = document.querySelector('#addBook');
-//const newBooksForm = document.querySelector('#newBookform');
 const main = document.querySelector('main');
 
 let book1 = {
@@ -26,6 +25,7 @@ let book3 = {
 
 // Array to store book objects
 let myLibrary = [book1, book2, book3];
+//let myLibrary = [];
 
 // Constructor for the book object
 class Book {
@@ -40,19 +40,67 @@ class Book {
     }
 }
 
-// Add book to the library array
-function addBookToLibrary(){
-    let title = prompt('Book name');
-    let author = prompt('Book author');
-    let pages = prompt('Pages number');
-    let isRead = function() {
-        if(confirm('Have you read this?')){
-            return true;
-        } else {
-            return false;
+// Add a DOM form for adding new books
+addBookButton.addEventListener('click', () => {       
+    let form = document.createElement('form');
+    form.setAttribute('onSubmit', 'return false');
+    form.setAttribute('id', 'newBookForm');
+
+    let titleName = document.createElement('p');
+    titleName.innerHTML = 'Title';
+    form.appendChild(titleName);    
+
+    let title = document.createElement('input');
+    title.setAttribute('type', 'text');
+    title.required = true;
+    form.appendChild(title);
+
+    let authorName = document.createElement('p');
+    authorName.innerHTML = 'Author';
+    form.appendChild(authorName);
+
+    let author = document.createElement('input');
+    author.setAttribute('type', 'text');
+    author.required = true;
+    form.appendChild(author);
+
+    let pagesName = document.createElement('p');
+    pagesName.innerHTML = 'Pages';
+    form.appendChild(pagesName);
+
+    let pages = document.createElement('input');
+    pages.setAttribute('type', 'number');
+    pages.required = true;
+    form.appendChild(pages);
+
+    let isReadName = document.createElement('p');
+    isReadName.innerHTML = 'Have you read this book?';
+    form.appendChild(isReadName);
+
+    let isRead = document.createElement('input');
+    isRead.setAttribute('type', 'checkbox');
+    form.appendChild(isRead);    
+
+    let addButton = document.createElement('input');
+    addButton.setAttribute('type', 'submit');    
+    addButton.setAttribute('id', 'finishBookAdd');
+    addButton.value = 'Add Book';  
+    form.appendChild(addButton);
+
+    main.appendChild(form);
+
+    addButton.addEventListener('click', () => {    
+        if((title.value !== '') && (author.value != '') && (pages.value !== '')){   
+            addBookToLibrary(title.value, author.value, pages.value, isRead.checked)
+            main.removeChild(form);
+            readBooks();
         }
-    }
-    let newBook = new Book(title, author, pages, isRead());
+    });    
+});
+
+// Add book to the library array
+function addBookToLibrary(title, author, pages, isRead){
+    let newBook = new Book(title, author, pages, isRead);
     myLibrary.push(newBook);
 }
 
@@ -74,53 +122,6 @@ function readBooks(){
         isRead.style.textAlign = 'center';
     }
 }
-
-// Add a DOM form for adding new books
-addBookButton.addEventListener('click', () => {       
-    let form = document.createElement('div');
-    form.setAttribute('id', 'newBookForm');
-
-    let titleName = document.createElement('p');
-    titleName.innerHTML = 'Title';
-    form.appendChild(titleName);    
-
-    let title = document.createElement('input');
-    form.appendChild(title);
-
-    let authorName = document.createElement('p');
-    authorName.innerHTML = 'Author';
-    form.appendChild(authorName);
-
-    let author = document.createElement('input');
-    form.appendChild(author);
-
-    let pagesName = document.createElement('p');
-    pagesName.innerHTML = 'Pages';
-    form.appendChild(pagesName);
-
-    let pages = document.createElement('input');
-    form.appendChild(pages);
-
-    let isReadName = document.createElement('p');
-    isReadName.innerHTML = 'Have you read this book?';
-    form.appendChild(isReadName);
-
-    let isRead = document.createElement('input');
-    isRead.setAttribute('type', 'checkbox');
-    form.appendChild(isRead);    
-
-    let addButton = document.createElement('button');
-    addButton.setAttribute('id', 'finishBookAdd');
-    addButton.innerHTML = 'Add Book';  
-    form.appendChild(addButton);
-
-    main.appendChild(form);
-
-
-    // add required fields
-    // add actual object creation
-    // add button functionality
-});
 
 document.addEventListener('keydown', function(event){
 	if(event.key === 'Escape'){
